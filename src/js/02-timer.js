310147d90b6fc8a60e8197eefdeb87b5488944af
AlonaStasyshyna <alena.stasishyna28@gmail.com>
2022-09-07 23:59:32 +0300

    Add task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,44 +5,56 @@ const inputForDate = document.querySelector('#datetime-picker');
 const btnStart = document.querySelector('button[data-start]');
 btnStart.setAttribute('disabled', 'true');
 
+const timerBlock = document.querySelector('.timer');
+const timerFields = document.querySelectorAll('.field');
+const timerValues = document.querySelectorAll('.value');
+const timerLabels = document.querySelectorAll('.label');
 const timerDays = document.querySelector('span[data-days]');
 const timerHours = document.querySelector('span[data-hours]');
 const timerMinutes = document.querySelector('span[data-minutes]');
 const timerSeconds = document.querySelector('span[data-seconds]');
+
+const SELECTEDDATE_KEY = 'selected-date';
+let timerStart = null;
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-
   onClose(selectedDates) {
     // console.log(selectedDates[0]);
-
     if (new Date() >= selectedDates[0]) {
       return window.alert('Please choose a date in the future');
     } else {
+      localStorage.setItem(SELECTEDDATE_KEY, selectedDates[0]);
       btnStart.disabled = false;
-      btnStart.addEventListener('click', () => {
-        const timerStart = setInterval(() => {
-          const deltaTime = selectedDates[0] - new Date();
-          const { days, hours, minutes, seconds } = convertMs(deltaTime);
-
-          timerSeconds.textContent = `${seconds}`;
-          timerMinutes.textContent = `${minutes}`;
-          timerHours.textContent = `${hours}`;
-          timerDays.textContent = `${days}`;
-        }, 1000);
-
-        // if (deltaTime < 1) {
-        //   clearInterval(timerStart);
-        // }
-      });
     }
   },
 };
 
 const fp = flatpickr(inputForDate, options);
+
+btnStart.addEventListener('click', onStartClick);
+
+function onStartClick() {
+  timerStart = setInterval(() => {
+    const selectedDate = new Date(localStorage.getItem(SELECTEDDATE_KEY));
+    const deltaTime = selectedDate - new Date();
+    const { days, hours, minutes, seconds } = convertMs(deltaTime);
+
+    timerSeconds.textContent = `${seconds}`;
+    timerMinutes.textContent = `${minutes}`;
+    timerHours.textContent = `${hours}`;
+    timerDays.textContent = `${days}`;
+
+    if (deltaTime < 1000) {
+      clearInterval(timerStart);
+      localStorage.removeItem(SELECTEDDATE_KEY);
+      return;
+    }
+  }, 1000);
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -72,3 +84,21 @@ function addLeadingZero(value) {
 
   return String(value);
 }
+
+// Styles
+timerBlock.style.display = 'flex';
+timerBlock.style.marginTop = '15px';
+timerBlock.style.gap = '15px';
+timerFields.forEach(field => {
+  field.style.display = 'flex';
+  field.style.flexDirection = 'column';
+  field.style.alignItems = 'center';
+});
+timerValues.forEach(value => {
+  value.style.fontSize = '32px';
+  value.style.lineHeight = '1';
+});
+timerLabels.forEach(label => {
+  label.style.fontSize = '14px';
+  label.style.lineHeight = '1,2';
+});
